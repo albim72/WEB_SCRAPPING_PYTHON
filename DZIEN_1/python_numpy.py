3efec8c0429d18989dfc2ac49a4a6f49c3610d99
albim72 <marcin.albiniak72@gmail.com>
2025-08-25 12:15:47 +0200

    Create python_numpy.py

---
+++ DZIEN_1/python_numpy.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+print(f"tablica np: {a}")
+print(f"typ tablicy: {type(a)}")
+print(f"rozmiar tablicy: {a.size}")
+print(f"rozmiar pierwszego elementu: {a.itemsize}")
+print(f"typ elementu: {a.dtype}")

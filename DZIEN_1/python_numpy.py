3efac55f6d8eadecaecf75f3a035e3f0582cd116
albim72 <marcin.albiniak72@gmail.com>
2025-08-25 13:12:10 +0200

    Update python_numpy.py

--- DZIEN_1/python_numpy.py
+++ DZIEN_1/python_numpy.py
@@ -14,3 +14,18 @@ print(f"arange: {b}")
 
 c = np.linspace(0,1,5)
 print(f"linspace: {c}")
+
+#operacje wektorowe (zamiast pętli)
+x = np.array([1,2,3])
+y = np.array([11,21,31])
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(y//x)
+
+a = np.array([[1,2,3],[4,5,6]])
+b = np.array([[11,21,31],[41,51,61]])
+c = np.array([[111,211,311],[411,511,611]])
+print(a)
+print(a+b+c)

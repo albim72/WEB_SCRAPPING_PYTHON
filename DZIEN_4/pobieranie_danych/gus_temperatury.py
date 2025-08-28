63e608a7d6c485690b261e6cdf7156a4dd0921d2
albim72 <marcin.albiniak72@gmail.com>
2025-08-28 11:19:51 +0200

    Update gus_temperatury.py

--- DZIEN_4/pobieranie_danych/gus_temperatury.py
+++ DZIEN_4/pobieranie_danych/gus_temperatury.py
@@ -1,3 +1,19 @@
+import requests, pandas as pd
+
+url = "https://bdl.stat.gov.pl/api/v1/variables"
+params = {
+    "search": "temperatura", 
+    "format": "json", 
+    "lang": "pl", 
+    "page-size": 50
+}
+r = requests.get(url, params=params, timeout=15)
+r.raise_for_status()
+data = r.json()
+
+for v in data["results"]:
+    print(v["id"], v["name"])
+
 unit_id = "000000000000"   # Polska
 var_id = 12345             # poprawny ID z wyszukiwania
 
